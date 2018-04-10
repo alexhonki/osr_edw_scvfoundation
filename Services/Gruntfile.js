2f749e1b20c946bac0d34f9b57aac510bd216e94
D043051 <alexander.bauer@sap.com>
2018-04-11 07:35:32 +1000

    Adapt eslint config file name in grunt file

--- Services/Gruntfile.js
+++ Services/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON("package.json"),
         "eslint": {
             options: {
-                configFile: ".eslintrc.json"
+                configFile: ".eslintrc"
             },
             target: ["*.js", "lib/**.js"]
         },
