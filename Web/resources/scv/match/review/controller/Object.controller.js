92b46f50b6dbddc53272584f37433dd1e21d9c7d
Stefanus.Husin <stefanus.husin@treasury.qld.gov.au>
2018-08-13 16:50:11 +1000

    Using unbind method for detail table

--- Web/resources/scv/match/review/controller/Object.controller.js
+++ Web/resources/scv/match/review/controller/Object.controller.js
@@ -123,7 +123,7 @@ sap.ui.define([
 
 			//enable busy indicator for the main table
 			this._setBusyIndicatorForMainTable(true);
-			this.getView().byId("detailsTable1").removeAllRows();
+			this.getView().byId("detailsTable1").unbindRows();
 
 			//clean table binding
 
