0cb3de0b85d04d25417ece4adfeffd5552745d21
stefanusz <Stefanus.Husin@treasury.qld.gov.au>
2018-08-23 11:48:47 +1000

    Add active procedures & active statements CV from EDW PLATFORM, Add route match on the worklist, add an interval checker poc

--- Web/resources/scv/match/supervisor/controller/BaseController.js
+++ Web/resources/scv/match/supervisor/controller/BaseController.js
@@ -54,6 +54,50 @@ sap.ui.define([
 				oViewModel.getProperty("/shareSendEmailSubject"),
 				oViewModel.getProperty("/shareSendEmailMessage")
 			);
+		},
+
+		/**
+		 * Generic function to initialize a dialog.
+		 * @param  {String} sDialogName     The name of the dialog fragment file.
+		 * @param  {String} sDialogPath     The path where the dialog fragment file located in.
+		 * @return {sap.ui.core.Control}    Dialog control.
+		 */
+		initializeDialog: function(sDialogName, sDialogPath, sControllerPath) {
+
+			let dialogController = sap.ui.controller(sControllerPath);
+
+			// assign this controller (the controller from the calling method) to the dialog controller
+			// to have access to the controller that call this, in case we want to access anything inside it.
+			dialogController.ownerController = this;
+
+			let sDialogId = this.makeRandomId();
+			sDialogId += sDialogName;
+			dialogController.dialog = sap.ui.xmlfragment(sDialogId, sDialogPath + sDialogName, dialogController);
+
+			dialogController.dialog = sap.ui.xmlfragment(sDialogName, sDialogPath + sDialogName, dialogController);
+
+			// need to assign the controller to the dialog to be able to access the data in case the dialog exists
+			dialogController.dialog.controller = dialogController;
+
+			// add dialog as dependent.
+			this.getView().addDependent(dialogController.dialog);
+
+			return dialogController.dialog;
+		},
+
+		/**
+		 * Generate random id base on letters and 8 chars.
+		 * @return {[type]} [description]
+		 */
+		makeRandomId: function() {
+			let text = "";
+			let sPossible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+			for (let i = 0; i < 8; i++) {
+				text += sPossible.charAt(Math.floor(Math.random() * sPossible.length));
+			}
+
+			return text;
 		}
 
 	});
